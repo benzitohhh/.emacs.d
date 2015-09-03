3851d79df66f528dc75f87d91cded4210b3c1343
Ben Immanuel <ben.immanuel@aistemos.com>
2015-09-03 15:39:15 +0100

    fix js2 idle highlight

--- init.el
+++ init.el
@@ -300,16 +300,14 @@
 (setq sgml-basic-offset 2)
 (add-hook 'sgml-mode-hook 'my-coding-hook)
 
-;; javascript
+;; javascript (i.e. js2-mode also sets js-mode, so only need to bind stuff to js-mode)
 (setq js-indent-level 2)
 (setq js2-basic-offset 2)
 (setq js2-bounce-indent-p t) ;; hmmmm maybe this should be t? Or nil?
 (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json" . js-mode))
 (add-hook 'js-mode-hook 'my-coding-hook)
-(add-hook 'js2-mode-hook 'my-coding-hook)
 (add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (eval-after-load 'tern
    '(progn
       (require 'tern-auto-complete)
