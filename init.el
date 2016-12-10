57bc3e68f3eb4266f76c9f624817a2c06ae2b38c
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2016-12-10 17:30:30 +0000

    tabs mode

--- init.el
+++ init.el
@@ -384,10 +384,11 @@
   (setq sgml-basic-offset size)
   (setq css-indent-offset size))
 
-(defun tab-space-toggle ()
-  "Toggle between using tabs/spaces"
+(defun tabs-please ()
+  "Use tabs rather than spaces"
   (interactive)
-  (setq-default indent-tabs-mode (not indent-tabs-mode)))
+  (setq-default indent-tabs-mode t)
+  (web-mode-use-tabs))
 
 ;; C and C++
 (setq c-basic-offset 4)
