ced8a122ada5da65a4c0333bafca4a26827a0629
Ben Immanuel <ben.immanuel@aistemos.com>
2018-07-03 11:31:42 +0100

    fix ack settings

--- init.el
+++ init.el
@@ -208,10 +208,13 @@
 (grep-apply-setting 'grep-command "grep -nHR --include \\*.js -e somePattern .")
 
 ;; full-ack
+(require 'full-ack)
 (autoload 'ack-same "full-ack" nil t)
 (autoload 'ack "full-ack" nil t)
 (add-to-list 'ack-project-root-file-patterns "frontend-service") ;; regex for default directory to search
 (setq ack-prompt-for-directory t) ;; always ask for directory before doing an ack search
+
+
 
 ;; Visual regex
 (require 'visual-regexp)
